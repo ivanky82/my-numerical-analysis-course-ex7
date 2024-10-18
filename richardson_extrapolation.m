1e28b32b18d34f1eff811794c1272368c2fefdfd
Ivanky Saputra <ivanky82@gmail.com>
2024-10-18 11:25:12 +0700

    Create richardson_extrapolation.m

---
+++ richardson_extrapolation.m
@@ -0,0 +1,19 @@
+function out = richardson_extrapolation(f,x,h,n)
+%RICHARDSON_EXTRAPOLATION Mencari hampiran turunan dari suatu fungsi dengan menggunakan
+%tabel ekstrapolasi Richardson
+%   out = richardson_extrapolation(f,x,h,n) menerima input fungsi f, titik x, step size h dan
+% 	n yang merupakan banyaknya baris/kolom pada tabel extrapolasi. Semakin besar n semakin
+%   akurat estimasinya
+tab = zeros(n,n);
+% ===================== KODENYA MULAI DARI SINI============================
+% ******************TULIS KODE DI BAWAH INSTRUKSI*************************
+% ------------------------------------------------------------------------
+
+
+
+
+
+
+% =========================================================================
+out = tab;
+end
